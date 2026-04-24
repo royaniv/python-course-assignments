031623d9610bf48a38086fe9618740becdd3c327
royy <royaniv@gmail.com>
2026-04-24 18:17:51 +0300

    Create a new file named "Drake Equation.py" in the "day02" directory with the following content:

---
+++ day02/Drake Equation.py
@@ -0,0 +1,25 @@
+# The Drake Equation
+def N = R_Star * f_p * n_e * f_i * f_c * L 
+from operator import sub
+
+#R_Star: Average rate of star formation.
+
+#f_p: Fraction of stars with planetary systems.
+
+#n_e: Average number of planets per system suitable for life.
+
+#f_l: Fraction of suitable planets where life develops.
+
+#f_i: Fraction of life-bearing planets where intelligent life emerges.
+
+#f_c: Fraction of civilizations releasing detectable signals.
+
+#L: The lifespan of such civilizations.
+
+input("Enter the average rate of star formation (R_Star): ")
+input("Enter the fraction of stars with planetary systems (f_p): ")
+input("Enter the average number of planets per system suitable for life (n_e): ")
+input("Enter the fraction of suitable planets where life develops (f_l): ")
+input("Enter the fraction of life-bearing planets where intelligent life emerges (f_i): ")
+input("Enter the fraction of civilizations releasing detectable signals (f_c): ")
+input("Enter the lifespan of such civilizations (L): ")
