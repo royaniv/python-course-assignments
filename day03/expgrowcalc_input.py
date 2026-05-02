551d310d3c4d969b77fa312f20a0419848cfd728
royy <royaniv@gmail.com>
2026-05-02 13:29:58 +0300

    changing file names to include .py extension and adding function call in expgrowcalc_input.py

---
+++ day03/expgrowcalc_input.py
@@ -0,0 +1,12 @@
+# exponential growth calculation using a function and input
+
+from expgrowfunct import expgrowth
+
+if __name__ == "__main__":
+    N0 = float(input("initial amount (N0): "))
+    r = float(input("growth rate (r): "))
+    t = float(input("time (t): "))
+    k = float(input("decay rate (k): "))
+
+    final_amount = expgrowth(N0, r, t, k)
+    print(f"Final amount (N) after time t: {final_amount}")
