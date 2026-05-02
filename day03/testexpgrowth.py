3db2aed772aa082de7f8eca420ddfd28f74d5f65
royy <royaniv@gmail.com>
2026-05-02 18:57:07 +0300

    adding tests

--- day03/testexpgrowth.py
+++ day03/testexpgrowth.py
@@ -1,6 +1,7 @@
 #examples for assert statements in expgrowth calculation input, sysargv, GUI
 #for input
 
+import expgrowcalc_GUI
 import expgrowcalc_input
 import expgrowcalc_sysargv
 
@@ -11,3 +12,8 @@ def test1():
 #for sysargv
 def test2():
     assert expgrowcalc_sysargv(2, 0.05, 10, 0.02) == 2.6997176151520064
+
+#for GUI
+def test3():
+    assert expgrowcalc_GUI(2, 0.05, 10, 0.02) == 2.6997176151520064
+    
