6b46206332329bab0309f2dd359abd42fde2a3c4
royy <royaniv@gmail.com>
2026-05-02 16:23:21 +0300

    created a better sys argv

---
+++ day03/expgrowcalc_sysargv.py
@@ -0,0 +1,18 @@
+import sys
+
+from expgrowfunct import expgrowth
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: python expgrowcalc_sysargv.py N0 r t k")
+        print("Example: python expgrowcalc_sysargv.py 100 0.05 10 0.02")
+        sys.exit(1)
+
+    N0 = float(sys.argv[1])
+    r = float(sys.argv[2])
+    t = float(sys.argv[3])
+    k = float(sys.argv[4])
+
+    final_amount = expgrowth(N0, r, t, k)
+    print(f"Final amount (N) after time t: {final_amount}")
