705ed5cb94214d455628ec0a17c4a35ee063a4d5
royy <royaniv@gmail.com>
2026-05-14 13:06:22 +0300

    amending commit message

--- day05/README.md
+++ day05/README.md
@@ -17,6 +17,16 @@ The program:
 - detects composome blocks
 - counts red and non-red regions
 
+ It should identify 4 composomes which it does.
+
+
+ The program detected 4 major composome blocks along the diagonal of the carpet plot.
+ However, the first and fourth composomes appear to belong to the same composome family because there are strong red/yellow regions connecting them away from the diagonal.
+
+ This indicates that these two composomes have similar compositions even though they appear in different positions on the diagonal. Writing into the program the ability to detect this seemed too advanced to me.
+
+ I also wanted the counts to add clarity that it actually checks the whole width to make sure it goes over it all since the image is 366 pixels wide this seems to result correctly when adding up red and non-red regions.
+
 ---
 
 ## Files
--- day05/image_analysis.py
+++ day05/image_analysis.py
@@ -3,9 +3,13 @@ from PIL import Image
 
 def detect_composomes(image_file):
 
+    # open image
     img = Image.open(image_file)
+
+    # convert image to RGB
     img = img.convert("RGB")
 
+    # image size
     width, height = img.size
 
     composomes = 0
@@ -15,38 +19,44 @@ def detect_composomes(image_file):
 
     inside_composome = False
 
+    # move along diagonal
     for i in range(width):
 
         red_total = 0
         blue_total = 0
 
-        # check around diagonal
+        # check nearby pixels around diagonal
         for offset in range(-25, 26):
 
             x = i + offset
             y = i
 
+            # skip invalid positions
             if x < 0 or x >= width:
                 continue
 
+            # get pixel color
             r, g, b = img.getpixel((x, y))
 
             red_total += r
             blue_total += b
 
-        # strongly red/yellow region
+        # strong red/yellow region
         if red_total > blue_total + 4000:
 
             red_regions += 1
 
+            # count composome only once
             if not inside_composome:
 
                 composomes += 1
+
                 inside_composome = True
 
         else:
 
             non_red_regions += 1
+
             inside_composome = False
 
     return composomes, red_regions, non_red_regions
@@ -59,5 +69,7 @@ if __name__ == "__main__":
     )
 
     print("Composomes detected:", composomes)
+
     print("Red diagonal regions:", red_regions)
+
     print("Non-red diagonal regions:", non_red_regions)
